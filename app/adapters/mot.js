28c81f622b1db5ead1816cf25d08d1a700abda7a
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2016-06-03 15:07:12 +0100

    fix(config): import application config (#4)

--- app/adapters/mot.js
+++ app/adapters/mot.js
@@ -1,4 +1,7 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import config from '../config/environment';
+
+console.log(config);
 
 export default JSONAPIAdapter.extend({
   namespace: 'api/v1',
