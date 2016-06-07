efcd01d9ee5c26b595234ed03bf5bde2e0097aa6
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-07 18:20:13 +0100

    refactor(mot): define route for creating new Mot
    The form for creating new Mots has been moved from the index page to a
    new page accessed via the 'new' route.

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function () {
   // Mots resource
   this.route('mots', function () {
+    this.route('new');
     this.route('mot', { path: ':id' });
   });
 
