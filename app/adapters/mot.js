28bacb06dfc5b7be990086e144b2809704a27eea
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-07 18:21:37 +0100

    fix(mot): define correct attributes for Mot model
    The attributes 'name' and 'description' were erroneously defined for the
    Mot resource. They've been removed and replace with 'content'.

--- app/adapters/mot.js
+++ app/adapters/mot.js
@@ -1,8 +1,6 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import config from '../config/environment';
 
-console.log(config);
-
 export default JSONAPIAdapter.extend({
   namespace: 'api/v1',
   host: config.API_HOST
