49addd8e9bca7ca71a0e1de9bc552a25527a9745
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-08 14:54:51 +0100

    refactor(mot): define actions to create new Mots from index page

--- app/routes/mots/index.js
+++ app/routes/mots/index.js
@@ -2,6 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('mot');
+    return Ember.RSVP.hash({
+      mots: this.store.findAll('mot'),
+      newMot: {},
+    });
+  },
+
+  setupController(controller, models) {
+    controller.set('mots', models.mots);
+    controller.set('newMot', models.newMot);
+  },
+
+  actions: {
+
+    saveMot(newMot) {
+      console.log('Mot to be created: ', newMot);
+      var noveauMot = this.store.createRecord('mot', newMot);
+      noveauMot.save().then(() => this.transitionTo('mots'));
+    },
   }
 });
