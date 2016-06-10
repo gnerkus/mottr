f33e9b3a8c6cd062ae58b3a58e86e102e3542254
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-10 15:00:49 +0100

    feat(index): integrate form and lastWord components in index page.
    The form no longer has a separate component and has been integrated into
    the index page. The lastWord component has also been integrated into the
    page.

--- app/routes/mots/index.js
+++ app/routes/mots/index.js
@@ -3,22 +3,31 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      mots: this.store.findAll('mot'),
-      newMot: {},
+      lastMot: { content: '?' },
+      currentMot: {}
     });
   },
 
   setupController(controller, models) {
-    controller.set('mots', models.mots);
-    controller.set('newMot', models.newMot);
+    controller.set('lastMot', models.lastMot);
+    controller.set('currentMot', models.currentMot);
   },
 
   actions: {
+    saveMot() {
+      // Fetch the current word typed by the user
+      const currentMot = this.controller.get('currentMot');
 
-    saveMot(newMot) {
-      console.log('Mot to be created: ', newMot);
-      var noveauMot = this.store.createRecord('mot', newMot);
-      noveauMot.save().then(() => this.transitionTo('mots'));
+      // Create the new Mot on the server
+      const mot = this.store.createRecord('mot', currentMot);
+      mot.save().then(() => {
+        // TODO: Play a sound here to indicate the player just sent a word.
+        // Set the last word to the current word's content
+        this.controller.set('lastMot', Ember.copy(currentMot));
+
+        // Reset the form
+        this.controller.set('currentMot', {});
+      });
     },
   }
 });
