9e72165fe7fe3f4d9f4aebddfcbc9cdec32bd08e
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-11 15:21:36 +0100

    feat(multiplayer): integrate websocket service into client app

--- app/routes/mots/index.js
+++ app/routes/mots/index.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  websocket: Ember.inject.service('websocket'),
+
+  init() {
+    this.get('websocket').socket.on('new word', (newWord) => {
+      const lastMot = {
+        content: newWord
+      };
+      // Set the last word to the current word's content
+      this.controller.set('lastMot', Ember.copy(lastMot));
+    });
+  },
+
   model() {
     return Ember.RSVP.hash({
       lastMot: { content: '?' },
@@ -21,9 +33,9 @@ export default Ember.Route.extend({
       // Create the new Mot on the server
       const mot = this.store.createRecord('mot', currentMot);
       mot.save().then(() => {
+        // Send the message to the Websocket server
+        this.get('websocket').sendMessage(currentMot.content);
         // TODO: Play a sound here to indicate the player just sent a word.
-        // Set the last word to the current word's content
-        this.controller.set('lastMot', Ember.copy(currentMot));
 
         // Reset the form
         this.controller.set('currentMot', {});
