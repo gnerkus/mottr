6e64c13ff41b7e3282498847d9f69b6db678b45d
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-24 09:14:36 +0100

    feat(user): define component for displaying user info
    The user-display component shows the user's avatar and score during a
    game session.

--- app/components/user-display.js
+++ app/components/user-display.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   animationEnd: 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+  classNames: ['row'],
   didUpdateAttrs() {
     const userScore = this.$().find('.user-score');
-    userScore.addClass('pulse').one(this.animationEnd, () => {
-      userScore.removeClass('pulse');
+    userScore.addClass('pulse high-score').one(this.animationEnd, () => {
+      userScore.removeClass('pulse high-score');
     });
   }
 });
