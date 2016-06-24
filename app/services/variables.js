48077e87c3d1e32dc7bc7532c586a65a5cbf9e36
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-24 09:19:34 +0100

    feat(variables): define service to store game information

--- app/services/variables.js
+++ app/services/variables.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  
+  gameID: '',
+  socketID: '',
+  gameState: 'inactive',
+  minUserCount: 2,
+  curPlayerID: '',
+  players: {}
 });
